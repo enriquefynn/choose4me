940048c796d23ea4ad965634c9941a9cb0a4d0d8
Fynn <enriquefynn@gmail.com>
2014-08-13 21:07:38 -0300

    Copy logos

---
+++ logo/copy_logo.sh
@@ -0,0 +1,5 @@
+  #!/usr/env bash
+  cp logo72.png ../Choose4Me/res/drawable-hdpi/ic_launcher.png
+  cp logo48.png ../Choose4Me/res/drawable-mdpi/ic_launcher.png
+  cp logo96.png ../Choose4Me/res/drawable-xhdpi/ic_launcher.png 
+  cp logo144.png ../Choose4Me/res/drawable-xxhdpi/ic_launcher.png 
